26ae093159fa56f5555fc107361f560a0a58491b
LitBowya <agbagabenjamin819@gmail.com>
2023-07-25 21:58:28 +0000

    fix an error in export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,9 +7,9 @@ import sys
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    u)).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    ser = requests.get(url + "users/{}".format(user_idtodos = requests.get(url + "todos", params={"userId": user_id}).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump({user_id: [{
